288c774efc8e073409560c4d44d6fa40a8118a62
SajanKumarSingh <kumarsajan0013@gmail.com>
2024-03-30 12:16:16 +0530

    Add project section  using Sass

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
  icon.onclick = function () {
    document.body.classList.toggle("dark-theme");
    if (document.body.classList.contains("dark-theme")) {
-     icon.src = "./assets/moon.png";
-   } else {
      icon.src = "./assets/sun.png";
+   } else {
+     icon.src = "./assets/moon.png";
    }
  };
  // Typing Animation
